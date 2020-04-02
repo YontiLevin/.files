c9fc8e050bbdd6de160cfba2e8151e2e222d8d88
yontilevin <yontilevin@gmail.com>
2020-04-02 19:03:01 +0300

    added zsh plugins

--- install.sh
+++ install.sh
@@ -6,6 +6,20 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	sudo chsh -s /bin/zsh $(whoami) 
 	sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+	# autosuggestion
+	export ub_version=$(lsb_release -r | awk '{print $2}')
+	sudo sh -c "echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-autosuggestions/xUbuntu_$ub_version/ /' > /etc/apt/sources.list.d/shells:zsh-users:zsh-autosuggestions.list"
+	wget -nv https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/xUbuntu_$ub_version/Release.key -O Release.key
+	sudo apt-key add - < Release.key
+	sudo apt-get update
+	sudo apt-get install zsh-autosuggestions
+
+	sudo sh -c "echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-syntax-highlighting/xUbuntu_$ub_version/ /' > /etc/apt/sources.list.d/shells:zsh-users:zsh-syntax-highlighting.list"
+	wget -nv https://download.opensuse.org/repositories/shells:zsh-users:zsh-syntax-highlighting/xUbuntu_ub_version/Release.key -O Release.key
+	sudo apt-key add - < Release.key
+	sudo apt-get update
+	sudo apt-get install zsh-syntax-highlighting
+
 	# solarized
 	sudo apt-get install dconf-cli
 	git clone https://github.com/aruhier/gnome-terminal-colors-solarized.git
@@ -31,5 +45,6 @@ cd ~
 
 # pyenv
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
 
 exec "$SHELL"
