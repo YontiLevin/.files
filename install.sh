f159736466d831a477a415ad13ae3e18023040d9
yontilevin <yontilevin@gmail.com>
2020-04-02 18:16:54 +0300

    fix 4 ubuntu installation

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	# zsh
-    	sudo apt-get update && sudo apt-get install -y zsh && \
-	sudo chsh -s /bin/zsh ubuntu && \
+    	sudo apt-get update 
+	sudo apt-get install -y zsh 
+	sudo chsh -s /bin/zsh ubuntu 
 	sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 	# solarized
