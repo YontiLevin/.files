b3976a74fdcf007f25032afee7d87dacc29d6c92
yontilevin <yontilevin@gmail.com>
2020-04-08 00:12:48 +0300

    removed solarized and p10k configure

--- install.sh
+++ install.sh
@@ -7,23 +7,12 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	sudo chsh -s /bin/zsh $(whoami) 
 	sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-	# solarized
-#	sudo apt-get install dconf-cli
-#	git clone --depth=1 https://github.com/aruhier/gnome-terminal-colors-solarized.git
-#	cd gnome-terminal-colors-solarized
-#	./install.sh
-
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         # zsh
 	brew install zsh
 	chsh -s $(which zsh)
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 	
-	# solarized
-	cd ~/Documents;\
-	git clone --depth=1 https://github.com/altercation/solarized
-	echo "go to Terminal > Preferences and import the color scheme"
-
 else
 	echo "unknown OSTYPE: $OSTYPE" 
 fi
@@ -48,6 +37,5 @@ sudo ln -s .dotfiles/zsh/.zshrc .zshrc
 
 # power10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-p10k configure
 
 exec "$SHELL"
